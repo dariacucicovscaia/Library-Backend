d8e8495a4acaf8a1109218406ffd7da1ab4ad288
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-07-25 17:25:17 +0300

    First configuration
    1. Hibernate and properties helpers
    2. GenericDao

---
+++ src/main/java/dao/DAOAbstractImpl.java
@@ -0,0 +1,55 @@
+package dao;
+import com.google.common.base.Preconditions;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+import java.io.Serializable;
+import java.util.List;
+@Repository
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public abstract class DAOAbstractImpl<T extends Serializable>  implements IGenericDao<T> {
+    private Class<T> clazz;
+    @Autowired
+    protected SessionFactory sessionFactory;
+    public final void setClazz(final Class<T> clazzToSet) {
+        clazz = Preconditions.checkNotNull(clazzToSet);
+    }
+
+    // API
+    public T findOne(final long id) {
+        return (T) getCurrentSession().get(clazz, id);
+    }
+
+    public List findAll() {
+        return getCurrentSession().createQuery("from " + clazz.getName()).list();
+    }
+
+    public T create(final T entity) {
+        Preconditions.checkNotNull(entity);
+        getCurrentSession().saveOrUpdate(entity);
+        return entity;
+    }
+
+    public T update(final T entity) {
+        Preconditions.checkNotNull(entity);
+        return (T) getCurrentSession().merge(entity);
+    }
+
+    public void delete(final T entity) {
+        Preconditions.checkNotNull(entity);
+        getCurrentSession().delete(entity);
+    }
+
+    public void deleteById(final long entityId) {
+        final T entity = findOne(entityId);
+        Preconditions.checkState(entity != null);
+        delete(entity);
+    }
+
+    protected Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+}
