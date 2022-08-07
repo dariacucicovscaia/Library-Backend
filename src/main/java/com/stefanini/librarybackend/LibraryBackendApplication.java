1fb85be68987fddd1a70bacb0d9632fb1f415b67
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-08-07 11:49:04 +0300

    Removed useless imports

--- src/main/java/com/stefanini/librarybackend/LibraryBackendApplication.java
+++ src/main/java/com/stefanini/librarybackend/LibraryBackendApplication.java
@@ -3,11 +3,6 @@ package com.stefanini.librarybackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @SpringBootApplication
@@ -15,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class LibraryBackendApplication {
 
     public static void main(String[] args) {
-       SpringApplication.run(LibraryBackendApplication.class, args);
+        SpringApplication.run(LibraryBackendApplication.class, args);
     }
 
 }
