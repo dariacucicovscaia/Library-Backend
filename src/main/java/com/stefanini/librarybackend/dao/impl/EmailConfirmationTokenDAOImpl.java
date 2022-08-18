84b448a79b3d5aa3ccb0b1b6d6cf970ea72d5334
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-08-18 15:36:23 +0300

    Added @repository

--- src/main/java/com/stefanini/librarybackend/dao/impl/EmailConfirmationTokenDAOImpl.java
+++ src/main/java/com/stefanini/librarybackend/dao/impl/EmailConfirmationTokenDAOImpl.java
@@ -2,7 +2,9 @@ package com.stefanini.librarybackend.dao.impl;
 
 import com.stefanini.librarybackend.dao.EmailConfirmationTokenDAO;
 import com.stefanini.librarybackend.domain.ConfirmationToken;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class EmailConfirmationTokenDAOImpl extends DAOAbstractImpl<ConfirmationToken> implements EmailConfirmationTokenDAO<ConfirmationToken> {
 
     public EmailConfirmationTokenDAOImpl() {
