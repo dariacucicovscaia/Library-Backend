9c240dc36faabdaba9a36ada535e121a4b0c3344
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-08-18 19:19:37 +0300

    Added java docs

--- src/main/java/com/stefanini/librarybackend/dao/EmailConfirmationTokenDAO.java
+++ src/main/java/com/stefanini/librarybackend/dao/EmailConfirmationTokenDAO.java
@@ -1,7 +1,5 @@
 package com.stefanini.librarybackend.dao;
 
 
-import com.stefanini.librarybackend.domain.ConfirmationToken;
-
 public interface EmailConfirmationTokenDAO<ConfirmationToken> extends IGenericDao<ConfirmationToken> {
 }
--- src/main/java/com/stefanini/librarybackend/service/EmailConfirmationTokenService.java
+++ src/main/java/com/stefanini/librarybackend/service/EmailConfirmationTokenService.java
@@ -2,6 +2,14 @@ package com.stefanini.librarybackend.service;
 
 import com.stefanini.librarybackend.domain.ConfirmationToken;
 
+/**
+ * This interface perform operations with email confirmation token that should be created after registration.
+ * After saving confirmation token the user will be inactive.
+ * That means that user cannot log in account before he will activate his account with token
+ * @author dcuciuc
+ * @since 0.1
+ * @version 0.1
+ */
 public interface EmailConfirmationTokenService {
     void saveConfirmationToken(ConfirmationToken confirmationToken);
 }
